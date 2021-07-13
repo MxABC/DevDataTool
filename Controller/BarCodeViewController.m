891f33c36a88563be9106b0c3e25d42b98e3873f
MxABC <lbxia200912227@foxmail.com>
2021-07-13 09:31:25 +0800

    update wifi qr

--- Controller/BarCodeViewController.m
+++ Controller/BarCodeViewController.m
@@ -45,6 +45,34 @@
 //    NSData *data = [NSData dataWithBytes:buffer length:strlen(buffer)];
 //    NSLog( @"hex:%@",[self hexString:data] );
     
+    
+    //wifi 二维码
+    
+    /*
+     
+     https://www.chenyudong.com/archives/wifi-qrcode-in-home-android-and-ios.html
+     
+     WIFI:T:WPA;S:wifiname;P:wifipasswd;
+     
+     说明一下：
+
+     WIFI 表示这个是一个连接 WiFi 的协议
+     S 表示后面是 WiFi 的 SSID，wifiname 也就是 WiFi 的名称
+     P 表示后面是 WiFi 的密码，wifipasswd 是 WiFi 的密码
+     T 表示后面是密码的加密方式，WPA/WPA2 大部分都是这个加密方式，也使用WPA。如果写WPA/WPA2我的小米手机无法识别。
+     H 表示这个WiFi是否是隐藏的，直接打开 WiFi 扫不到这个信号。苹果还不支持隐藏模式
+     
+     iOS11 使用系统相机或二维码扫码功能均可扫描 会提示是否需要加入 xx wifi
+     
+     Android大部分手机自带相机均支持扫码加入wifi
+     
+     */
+    
+    str = @"WIFI:T:WPA;S:TP-LINK_B8E9;P:12345678;";
+    str = @"WIFI:T:WPA/WPA2;S:TP-LINK_B8E9;P:handkoo123456;";
+
+    data = [str dataUsingEncoding:NSUTF8StringEncoding];
+    
     NSImage *img = [self createQRWithData:data QRSize:CGSizeMake(200, 200)];
     self.barImageView.image = [self resizeImage2:img forSize:CGSizeMake(200, 200)];
     
@@ -71,7 +99,54 @@
     return arrayString.uppercaseString;
 }
 
+
 - (NSImage*)createQRWithData:(NSData*)data QRSize:(CGSize)size
+{
+    NSData *stringData = data;
+    
+    //生成
+    CIFilter *qrFilter = [CIFilter filterWithName:@"CIQRCodeGenerator"];
+    [qrFilter setValue:stringData forKey:@"inputMessage"];
+    [qrFilter setValue:@"H" forKey:@"inputCorrectionLevel"];
+        
+    NSBitmapImageRep *rep = [[NSBitmapImageRep alloc] initWithBitmapDataPlanes:NULL
+                                                                       pixelsWide:256
+                                                                       pixelsHigh:256
+                                                                    bitsPerSample:8
+                                                                  samplesPerPixel:4
+                                                                         hasAlpha:YES
+                                                                         isPlanar:NO
+                                                                   colorSpaceName:NSDeviceRGBColorSpace
+                                                                      bytesPerRow:4 * 256
+                                                                     bitsPerPixel:32];
+    NSGraphicsContext *g = [NSGraphicsContext graphicsContextWithBitmapImageRep:rep];
+    CGContextRef context = [g graphicsPort];
+    
+    CIContext *ciContext = [CIContext contextWithCGContext:context options:nil];
+    CGImageRef img = [ciContext createCGImage:[qrFilter outputImage] fromRect:[[qrFilter outputImage] extent]];
+    
+    CGContextSetInterpolationQuality(context, kCGInterpolationNone);
+    CGContextDrawImage(context, CGRectMake(0, 0, 256, 256), img);
+    CGImageRef output_image = CGBitmapContextCreateImage(context);
+    
+    
+    //画像保存
+//    NSString* path = [NSHomeDirectory() stringByAppendingPathComponent:@"Desktop/hoge.png"];
+//    
+//    NSBitmapImageRep* rep2 = [[NSBitmapImageRep alloc] initWithCGImage:output_image];
+//    NSData* PNGData = [rep2 representationUsingType:NSPNGFileType properties:nil];
+//    [PNGData writeToFile:path atomically:NO];
+//    //関連するアプリ（プレビューなど）で表示
+//    [[NSWorkspace sharedWorkspace] openFile:path];
+//    
+    
+    NSImage *codeImage = [self imageFromCGImageRef:output_image];
+
+    
+    return codeImage;
+}
+
+- (NSImage*)createQRWithData2:(NSData*)data QRSize:(CGSize)size
 {
     NSData *stringData = data;
     
@@ -86,6 +161,8 @@
     CGImageRef cgImage = [[CIContext contextWithOptions:nil] createCGImage:qrImage fromRect:qrImage.extent];
    
     NSImage *codeImage = [self imageFromCGImageRef:cgImage];
+    
+//    codeImage = [self resizeImage2:codeImage forSize:CGSizeMake(200, 200)];
     
     return codeImage;
 }
@@ -161,21 +238,15 @@
     CGContextRef imageContext = nil;
     
     NSImage* newImage = nil;
-    
+        
     imageRect.size.height = CGImageGetHeight(image);
-
-    imageRect.size.width = CGImageGetWidth(image);
+       imageRect.size.width = CGImageGetWidth(image);
     
-//    imageRect.size.height = 200;
-//
-//    imageRect.size.width = 200;
-    
-    newImage = [[NSImage alloc] initWithSize:imageRect.size];
+    newImage = [[NSImage alloc]initWithSize:imageRect.size];
     
     [newImage lockFocus];
         
     imageContext = (CGContextRef)[[NSGraphicsContext currentContext]
-                                  
                                   graphicsPort];
     
     CGContextDrawImage(imageContext, *(CGRect*)&imageRect, image);
